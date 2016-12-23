74d741b92332c2ea48d4d7f9c5237bc46b44a963
Luka Milic <lukam321@gmail.com>
2016-12-23 12:15:21 +0000

    finished while loop and applied dummy next state function to board

--- conway.py
+++ conway.py
@@ -8,5 +8,8 @@ iteration = 0
 size = 20
 for i in range(size):
     board.append([0 for i in range(size)])
-while (iteration < 10):
+
+while (iteration < 3):
     pprint(board)
+    board = generate_next_board_state(board)
+    iteration += 1
