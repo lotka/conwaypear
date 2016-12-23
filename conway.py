766854bea68671ba3a01eda6674ec3a67f5a07d0
Timothy Lim <timothy_lim@outlook.com>
2016-12-23 15:48:29 +0100

    Prettify printing, seed the first life-form

--- conway.py
+++ conway.py
@@ -5,11 +5,15 @@ def generate_next_board_state(board):
 
 board = []
 iteration = 0
-size = 20
+size = 8
 for i in range(size):
     board.append([0 for i in range(size)])
 
+#Seed the first life-form
+board[5][5] = 1
+
 while (iteration < 3):
     pprint(board)
+    print('\n')
     board = generate_next_board_state(board)
     iteration += 1
