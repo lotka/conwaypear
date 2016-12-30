5176b8d2e9515df048f2b61301d86df6b1ad54d4
Luka Milic <lukam321@gmail.com>
2016-12-30 11:45:00 +0000

    added rule 1

--- conway.py
+++ conway.py
@@ -1,11 +1,21 @@
 from pprint import pprint
 from copy import copy
 
+"""
+Conway Rules:
+1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+"""
+
 def generate_next_board_state(board):
     new_board = copy(board)
     for i in xrange(len(board)):
         for j in xrange(len(board)):
-            if count_alive_cells_near_cell(board, i, j): pass
+            count = count_alive_cells_near_cell(board, i, j)
+            if board[i][j] == 1:
+                if count < 2: new_board[i][j] = 0; # Rule 1
     return new_board
 
 def count_alive_cells_near_cell(board,x,y):
